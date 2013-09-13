5c6fda952a0502d48130d7c57665f0bd1a0a8282
MakerSquare Student <students@themakersquare.com>
2013-09-13 13:29:53 -0500

    12 Test cases all passing. To be implemented: keep track of sets, matches

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -17,6 +17,7 @@ describe Tennis::Game do
       p2 = game.player2
       p1.opponent = p2
       p2.opponent = p1
+
       expect(p1.opponent).to eq(p2)
       expect(p2.opponent).to eq(p1)
     end
@@ -25,9 +26,9 @@ describe Tennis::Game do
   describe '#wins_ball' do
     it 'increments the points of the winning player' do
       p1 = game.player1
-      game.wins_ball(p1,1)
+      game.wins_ball(1)
 
-      expect(game.player1.points).to eq(1)
+      expect(p1.points).to eq(1)
     end
   end
 end
@@ -84,5 +85,54 @@ describe Tennis::Player do
         expect(player.score).to eq('forty')
       end
     end
+
+    context "when points is 3 for each player" do
+      it "returns deuce" do
+        game = Tennis::Game.new
+        game.wins_ball(1)
+        game.wins_ball(2)
+        game.wins_ball(1)
+        game.wins_ball(2)
+        game.wins_ball(1)
+        game.wins_ball(2)
+
+        expect(game.player1.score).to eq('deuce')
+      end
+    end
+
+    context "when player1 has 4 and player2 has 3" do
+      it "player1.score returns advantage" do
+        game = Tennis::Game.new
+        game.wins_ball(1)#1
+        game.wins_ball(2)#1
+        game.wins_ball(1)
+        game.wins_ball(2)
+        game.wins_ball(1)#3
+        game.wins_ball(2)#3
+        game.wins_ball(1)#4
+
+        expect(game.player1.score).to eq('advantage')
+      end
+    end
+
+    context "when player1 has advantage and scores again" do
+      it "returns player1 is the winner" do
+        game = Tennis::Game.new
+        game.wins_ball(1)#1
+        game.wins_ball(2)#1
+        game.wins_ball(1)
+        game.wins_ball(2)
+        game.wins_ball(1)#3
+        game.wins_ball(2)#3
+        game.wins_ball(1)#4
+        game.wins_ball(1)#5
+        # p1: 5  p2: 3
+
+        expect(game.player2.win).to eq(false)
+        expect(game.player1.win).to eq(true)
+        expect(game.player1.games_won).to eq(1)
+      end
+    end
+
   end
 end
--- tennis.rb
+++ tennis.rb
@@ -10,19 +10,52 @@ module Tennis
       @player2.opponent = @player1
     end
 
-    def wins_ball(player,num)
-      player.record_won_ball!
+    def wins_ball(num)
+      if num == 1
+        @player1.record_won_ball!
+        if @player1.advantage
+          @player1.win = true
+          @player1.games_won += 1
+        end
+      elsif num == 2
+        @player2.record_won_ball!
+        if @player2.advantage
+          @player2.win = true
+          @player1.games_won += 1
+        end
+      end
+
+
+      @player1.deuce = false
+      @player2.deuce = false
+      @player1.advantage = false
+      @player2.advantage = false
+      point1 = @player1.points
+      point2 = @player2.points
+
+      if point1 > 2 and point2 > 2
+        if point1 == point2
+          @player1.deuce = true
+          @player2.deuce = true
+        elsif point1 > point2
+          @player1.advantage = true
+        elsif point2 > point1
+          @player2.advantage = true
+        end
+      end
     end
   end
 
   class Player
-    attr_accessor :points, :opponent
+    attr_accessor :points, :opponent, :deuce, :advantage, :win, :games_won
 
     def initialize
       @points = 0
       @score = ['love','fifteen','thirty','forty']
       @deuce = false
       @advantage = false
+      @win = false
+      @games_won = 0
     end
 
     # Increments the score by 1.
@@ -34,7 +67,14 @@ module Tennis
 
     # Returns the String score for the player.
     def score
-      @score[@points]
+      if (@deuce == false && @advantage == false)
+        @score[@points]
+      elsif @deuce
+        'deuce'
+      elsif @advantage
+        'advantage'
+      end
+        
     end
   end
 end
